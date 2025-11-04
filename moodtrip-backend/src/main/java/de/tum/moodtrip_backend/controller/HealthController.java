44261c35b6807eade74612f1683c0fff76dd9b70
Tianding_hou <Apollohoutianding@outlook.com>
2025-11-04 19:51:53 +0100

    86c6b8few: Add HealthController for backend health check, more feature on health endpoint to be added. Change OPENAI_key to env var.

---
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/controller/HealthController.java
@@ -0,0 +1,14 @@
+package de.tum.moodtrip_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HealthController {
+    @GetMapping("/custom-health")
+    public Mono<String> healthCheck() {
+        return Mono.just("backend is running");
+    }
+}
