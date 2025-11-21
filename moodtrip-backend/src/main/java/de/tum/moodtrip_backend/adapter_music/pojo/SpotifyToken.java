3e1633340c6e8690d4b70d40cf9d2df06f0f20f0
Tianding_hou <Apollohoutianding@outlook.com>
2025-11-21 10:40:58 +0100

    86c6mt1cv:refactor: clean up code formatting and remove unnecessary whitespace

---
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/adapter_music/pojo/SpotifyToken.java
@@ -0,0 +1,70 @@
+package de.tum.moodtrip_backend.adapter_music.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("spotify_tokens")
+public class SpotifyToken {
+    @Id
+    private Long userId;
+    @Column("access_token")
+    private String accessToken;
+    @Column("refresh_token")
+    private String refreshToken;
+    @Column("expires_in")
+    private Long expiresIn;
+    @Column("fetched_at")
+    private Long fetchedAt;
+
+    public SpotifyToken() {
+    }
+
+    public SpotifyToken(String accessToken, Long expiresIn, Long fetchedAt, String refreshToken, Long userId) {
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+        this.fetchedAt = fetchedAt;
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public Long getFetchedAt() {
+        return fetchedAt;
+    }
+
+    public void setFetchedAt(Long fetchedAt) {
+        this.fetchedAt = fetchedAt;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
