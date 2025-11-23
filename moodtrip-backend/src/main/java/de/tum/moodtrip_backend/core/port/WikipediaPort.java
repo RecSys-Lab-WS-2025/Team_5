88b128229d26330b5d21338ea0984620d2dcc150
zhuxu <zhuxu1304@gmail.com>
2025-11-23 16:33:19 +0100

    86c6m4pu7: fetch image url of the pois

--- moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/port/WikipediaPort.java
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/port/WikipediaPort.java
@@ -3,5 +3,8 @@ package de.tum.moodtrip_backend.core.port;
 import reactor.core.publisher.Mono;
 
 public interface WikipediaPort {
-    Mono<String> fetchSummaryForTags(String wikiTag);
+    Mono<String> fetchSummaryForTag(String wikiTag);
+
+    Mono<String> fetchImageUrl(String imageTag, String wikipediaTag, String wikidataId, String wikimediaCommonsTag);
+
 }
