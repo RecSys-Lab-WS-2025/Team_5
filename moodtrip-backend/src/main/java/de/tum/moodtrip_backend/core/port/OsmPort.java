8febde6b8f12c83ceb51c7b3be3cdafee8123539
zhuxu <zhuxu1304@gmail.com>
2025-11-23 23:05:29 +0100

    86c6m4qcn: remove unused overpass function

--- moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/port/OsmPort.java
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/port/OsmPort.java
@@ -2,14 +2,8 @@ package de.tum.moodtrip_backend.core.port;
 
 import de.tum.moodtrip_backend.core.model.POI;
 import de.tum.moodtrip_backend.core.model.POICategory;
-import de.tum.moodtrip_backend.core.model.Route;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface OsmPort {
-    Mono<Route> fetchRelationWithWays(long relationId);
-
-    Mono<Long> findAmenitiesAround(double lat, double lon, String routeType, long radiusMeters);
-
     Flux<POI> findAmenitiesAround(double lat, double lon, POICategory poiCategory, int radiusMeters);
 }
--- moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/service/OverpassService.java
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/core/service/OverpassService.java
@@ -4,12 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import de.tum.moodtrip_backend.adapter_osm.builder.PoiDescriptionBuilder;
 import de.tum.moodtrip_backend.core.model.POI;
 import de.tum.moodtrip_backend.core.model.POICategory;
-import de.tum.moodtrip_backend.core.model.Route;
 import de.tum.moodtrip_backend.core.port.OsmPort;
 import de.tum.moodtrip_backend.core.port.WikipediaPort;
 
@@ -23,10 +21,6 @@ public class OverpassService {
     public OverpassService(OsmPort osmPort, WikipediaPort wikipediaPort) {
         this.osmPort = osmPort;
         this.wikipediaPort = wikipediaPort;
-    }
-
-    public Mono<Route> getRoute(double lat, double lon, String routeType, long radiusMeters) {
-        return osmPort.findAmenitiesAround(lat, lon, routeType, radiusMeters).flatMap(osmPort::fetchRelationWithWays);
     }
 
     public Flux<POI> getPois(double lat, double lon, POICategory poiCategory, int radiusMeters) {
