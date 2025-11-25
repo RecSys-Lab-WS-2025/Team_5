5157f2b1a2991602a8aeb4f4c4d38452d7c02d5b
zhuxu <zhuxu1304@gmail.com>
2025-11-25 19:19:00 +0100

    86c6qfxgn: authorize using jwt token

---
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/security/JwtServerAuthenticationConverter.java
@@ -0,0 +1,43 @@
+package de.tum.moodtrip_backend.security;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+
+@Component
+public class JwtServerAuthenticationConverter implements ServerAuthenticationConverter {
+
+    private final JwtService jwtService;
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    public JwtServerAuthenticationConverter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Mono<Authentication> convert(ServerWebExchange exchange) {
+        return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+                .filter(header -> header.startsWith(BEARER_PREFIX))
+                .map(header -> header.substring(BEARER_PREFIX.length()))
+                .map(this::createAuthentication);
+    }
+
+    private JwtToken createAuthentication(String token) {
+        Long userId = jwtService.extractUserId(token);
+        String username = jwtService.extractUsername(token);
+
+        UserDetails userDetails = User.withUsername(username)
+                .password("") // not used
+                .authorities(Collections.emptyList()) // no roles yet
+                .build();
+
+        return new JwtToken(token, userId, userDetails);
+    }
+}
