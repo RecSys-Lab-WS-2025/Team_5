05461d05ab7a1885cf16e795c3eb40cc0c32475d
zhuxu <zhuxu1304@gmail.com>
2025-11-25 19:19:00 +0100

    86c6qfxgn: authorize using jwt token

---
+++ moodtrip-backend/src/main/java/de/tum/moodtrip_backend/security/JwtToken.java
@@ -0,0 +1,43 @@
+package de.tum.moodtrip_backend.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtToken extends AbstractAuthenticationToken {
+
+    private final String token;
+    private final Long userId;
+    private final UserDetails principal;
+
+    public JwtToken(String token, Long userId, UserDetails principal) {
+        super(principal != null ? principal.getAuthorities() : null);
+        this.token = token;
+        this.userId = userId;
+        this.principal = principal;
+        setAuthenticated(false);
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public UserDetails getPrincipal() {
+        return principal;
+    }
+
+    public JwtToken withAuthenticated(boolean authenticated) {
+        JwtToken authenticatedToken = new JwtToken(this.token, this.userId, this.principal);
+        authenticatedToken.setAuthenticated(authenticated);
+        return authenticatedToken;
+    }
+}
